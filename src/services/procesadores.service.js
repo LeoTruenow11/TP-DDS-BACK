9da06c71f4655552c9fd461143e79a9cd4dc9b07
Leonardo Truenow <113139520+LeoTruenow11@users.noreply.github.com>
2024-07-01 22:08:08 -0300

    Add files via upload

--- src/services/procesadores.service.js
+++ src/services/procesadores.service.js
@@ -1,192 +1,39 @@
-import sequelize from "../models/database.js";
-import { Op } from "sequelize";
+const URL = "http://localhost:4000/api/procesadores"
 
-// ====================GET====================
-const getProcesadores = async (filter) => {
+async function getByFilters(filter){
+    let apiUrl = filter ? `${URL}/?nombre=${filter}` : URL;
+    const res = await fetch(apiUrl,{method: 'GET'})
+
+    return await res.json()
+}
+
+async function saveProcesador(procesador) {
     try {
-        const whereQuery = {}
-
-        if (filter.nombre) {
-            whereQuery.nombre = { [Op.like]: `%${filter.nombre}%` };
+        const method = procesador.IdProcesador === undefined ? 'POST' : 'PUT';
+        const res = await fetch(URL, {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(procesador)
+        });
+        if (!res.ok) {
+            throw new Error(`Error saving procesador: ${res.statusText}`);
         }
-
-        const resultado = await sequelize.models.Procesadores.findAll({
-            where: whereQuery,
-            attributes: [
-                'IdProcesador',
-                'Nombre',
-                'Precio',
-                'Stock',
-                'FechaRecibido',
-                'Disponible'
-            ],            
-        })
-        return resultado.map(p => {
-            return {
-                IdProcesador: p.dataValues.IdProcesador,
-                Nombre: p.dataValues.Nombre,
-                Precio: p.dataValues.Precio,
-                Stock: p.dataValues.Stock,
-                FechaRecibido: p.dataValues.FechaRecibido,
-                Disponible: p.dataValues.Disponible
-            }
-        })
+        return await res.json();
     } catch (error) {
-        console.error("Error fetching procesadores:", error.message);
+        console.error("Error in saveProcesador:", error.message);
         throw error;
     }
 }
 
-// ====================GETBYID====================
-const getProcesadorById = async (idProcesador) => {
-    try {
-        const resultado = await sequelize.models.Procesadores.findOne({
-            attributes: [
-                'IdProcesador',
-                'Nombre',
-                'Precio',
-                'Stock',
-                'FechaRecibido',
-                'Disponible'
-            ],
-            where: { IdProcesador: idProcesador }
-        });
-
-        if (!resultado) {
-            return null;
-        }
-
-        return resultado.dataValues;
-    } catch (error) {
-        console.error('Error fetching Procesador by ID:', error.message);
-        throw error;
+async function deleteProcesador(IdProcesador) {
+    const res = await fetch(`${URL}/${IdProcesador}`, {
+        method: 'DELETE',
+    });
+    if (!res.ok) {
+        throw new Error(`Error deleting procesador: ${res.statusText}`);
     }
-};
-
-// ====================POST====================
-const insertarProcesador = async (newProcesador) => {
-    try {
-        const resultado = await sequelize.models.Procesadores.create({
-            Nombre: newProcesador.Nombre,
-            Precio: newProcesador.Precio,
-            Stock: newProcesador.Stock,
-            FechaRecibido: newProcesador.FechaRecibido,
-            Disponible: newProcesador.Disponible
-        });
-        return {
-            IdProcesador: resultado.dataValues.IdProcesador,
-            Nombre: resultado.dataValues.Nombre,
-            Precio: resultado.dataValues.Precio,
-            Stock: resultado.dataValues.Stock,
-            FechaRecibido: resultado.dataValues.FechaRecibido,
-            Disponible: resultado.dataValues.Disponible
-        };
-    } catch (error) {
-        console.error("Error inserting procesador:", error.message);
-        throw error;
-    }
-};
-
-// ====================PUT====================
-const editarProcesador = async (procesadorData) => {
-    try {
-        // Buscar el procesador existente por su ID
-        const procesadorExistente = await sequelize.models.Procesadores.findOne({
-            where: { IdProcesador: procesadorData.IdProcesador }
-        });
-
-        // Si no se encuentra el procesador, retornar null
-        if (!procesadorExistente) {
-            return null;
-        }
-
-        // Actualizar el procesador con el nuevo nombre
-        const updatedProcesador = await procesadorExistente.update(
-            {
-                Nombre: procesadorData.Nombre,
-                Precio: procesadorData.Precio,
-                Stock:  procesadorData.Stock,
-                FechaRecibido: procesadorData.FechaRecibido,
-                Disponible: procesadorData.Disponible
-            }
-        )
-        // Retornar el ID del procesador actualizado
-        return updatedProcesador.dataValues;
-        
-    } catch (error) {
-        // Manejar y mostrar cualquier error que ocurra
-        console.error("Error updating procesador:", error.message);
-        throw error;
-    }
-};
-
-const getProcesadorByName = async (nombre) => {
-    try {
-        const resultado = await sequelize.models.Procesadores.findOne({
-            attributes: [
-                'IdProcesador',
-                'Nombre',
-                'Precio',
-                'Stock',
-                'FechaRecibido',
-                'Disponible'
-            ],
-            where: { Nombre: { [Op.like]: `%${nombre}%` } }
-        });
-
-        if (!resultado) {
-            return null;
-        }
-
-        return resultado.dataValues;
-    } catch (error) {
-        console.error("Error fetching procesador by name:", error.message);
-        throw error;
-    }
-};
-
-
-// ====================DELETE====================
-const eliminarProcesador = async (procesadorData) => {
-    try {
-        console.log('Datos recibidos para eliminar:', procesadorData);
-        
-        // Verificar si hay notebooks asociadas con el procesador
-        const notebooksAsociadas = await sequelize.models.Notebooks.findAll({
-            where: { IdProcesador: procesadorData.IdProcesador }
-        });
-
-        if (notebooksAsociadas.length > 0) {
-            // No permitir eliminar el procesador si hay notebooks asociadas
-            return { error: 'No se puede eliminar el procesador porque está asociado a una o más notebooks.' };
-        }
-        
-        
-        const procesadorExistente = await sequelize.models.Procesadores.findOne({
-            where: { IdProcesador: procesadorData.IdProcesador }
-        });
-        
-        if (!procesadorExistente) {
-            console.log('Procesador no encontrado');
-            return null; // Película no encontrada
-        }
-        
-        await procesadorExistente.destroy();
-        return procesadorData;
-    } catch (error) {
-        console.error('Error al eliminar el procesador:', error.message);
-        throw error;
-    }
-};
-
-
-const procesadorService = {
-    getProcesadores,
-    getProcesadorById,
-    insertarProcesador,
-    editarProcesador,
-    getProcesadorByName,
-    eliminarProcesador
+    return await res.json();
 }
 
-export default procesadorService
+export default { getByFilters, saveProcesador, deleteProcesador }
+
